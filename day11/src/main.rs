74b34eacfc354456c4839e1672f5f6c31eded5e7
Joshy Orndorff <admin@joshyorndorff.com>
2024-12-11 21:00:19 -0500

    11.1 working cache, but I foolishly keyed my cache by sequence rather than individual rock

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = std::fs::read_to_string("./input.txt").expect("input file should exist");
     // let input = std::fs::read_to_string("./example.txt").expect("input file should exist");
@@ -15,9 +17,16 @@ fn main() {
     );
 
     // Solve part 1 by the recursive expansion method
+    // println!(
+    //     "Part 1 via recursive expansion: {}",
+    //     recursive_expansion(25, &starting_sequence).len()
+    // );
+
+    // Solve part 2 by recursive expansion with a cache.
+    let mut cache = HashMap::<(usize, Vec<String>), Vec<String>>::new();
     println!(
-        "Part 1 via recursive expansion: {}",
-        recursive_expansion(25, &starting_sequence).len()
+        "Part 2 via cached recursive expansion: {}",
+        recursive_expansion(75, &starting_sequence, &mut cache).len()
     );
 }
 
@@ -50,7 +59,7 @@ fn expand_single(rock: &str) -> Vec<String> {
 fn direct_expansion(generations: usize, starting_sequence: &Vec<String>) -> Vec<String> {
     let mut sequence = starting_sequence.clone();
 
-    for i in 1..=generations {
+    for _ in 1..=generations {
         // We don't want to be constantly resizing the vec, so allocate double the previous round.
         // This will always be enough and will almost always be too much.
         let mut next_gen = Vec::with_capacity(sequence.len() * 2);
@@ -67,19 +76,37 @@ fn direct_expansion(generations: usize, starting_sequence: &Vec<String>) -> Vec<
 
 /// Solves the rock problem in a depth first way fully expanding the first rock in the starting
 /// sequence before moving on to the second.
-/// This allows caching but I won't implement the caching until the basic algo is working first.
-fn recursive_expansion(generations: usize, starting_sequence: &Vec<String>) -> Vec<String> {
+/// This allows caching.
+fn recursive_expansion(
+    generations: usize,
+    starting_sequence: &Vec<String>,
+    cache: &mut HashMap<(usize, Vec<String>), Vec<String>>,
+) -> Vec<String> {
     // The terminating case is when we are asked for zero generations. Then we just return the rocks we were given.
     if generations == 0 {
         return starting_sequence.clone();
     }
 
+    // Check if the call we are making is already in the cache.
+    if let Some(cached_result) = cache.get(&(generations, starting_sequence.clone())) {
+        println!("CACHE HIT BABY!!!! {:?}", starting_sequence);
+        return cached_result.clone();
+    }
+
+    println!("cache miss {:?}", starting_sequence);
+
     let mut fully_expanded = Vec::new();
 
     for rock in starting_sequence {
-        let single_rock_expansion = recursive_expansion(generations - 1, &expand_single(rock));
+        let single_rock_expansion =
+            recursive_expansion(generations - 1, &expand_single(rock), cache);
         fully_expanded.extend(single_rock_expansion);
     }
+
+    cache.insert(
+        (generations, starting_sequence.clone()),
+        fully_expanded.clone(),
+    );
 
     fully_expanded
 }
